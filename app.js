6f793eb21caf89f1187d1ac901cc69f5bf828e61
codependent <joseantonio.inigo@gmail.com>
2014-04-23 13:49:22 +0200

    commit3

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 var Q = require('q');
 var fs = require('fs');
+var PATH = "./.gitignore";
+var PATH2 = "./.gitignore";
+var PATH3 = "./inexistent.txt";
+
+//Llamada normal asíncrona
+fs.readFile(PATH, 'utf8', function(err,data){
+	if(err){
+		console.error('**** fs.readFile PATH - error: ' +err);
+	}else{
+		console.log('**** fs.readFile PATH - dat: '+data);	
+		fs.readFile(PATH2, 'utf8', function(err2,data2){
+			if(err2){
+				console.error('**** fs.readFile PATH2 - error: ' +err2);
+			}else{
+				console.log('**** fs.readFile PATH2 - dat: '+data2);	
+				fs.readFile(PATH3, 'utf8', function(err3,data3){
+					if(err3){
+						console.error('**** fs.readFile PATH3 - error: ' +err3);
+					}else{
+						console.log('**** fs.readFile PATH3 - dat: '+data3);	
+					}	
+				});
+			}	
+		});
+	}	
+});
+
+/* Esto solo para syncs */
+Q(fs.readFileSync(PATH, 'utf8'))
+.then(function(data){
+	console.log('$$$$ Q(fs.readFileSync()) PATH - datos: '+data);
+	return Q(fs.readFileSync(PATH2, 'utf8'));
+})
+.then(function(data){
+	console.log('$$$$ Q(fs.readFileSync()) PATH2 - datos: '+data);
+	return Q(fs.readFileSync(PATH3, 'utf8'));
+})
+.then(function(data){
+	console.log('$$$$ Q(fs.readFileSync()) PATH3 - datos: '+data);
+})
+.fail(function(err){
+	console.error("$$$$ Q(fs.readFileSync()) - error: "+err);
+});
+
 var readFile = function (path) {
-	console.log('read '+path);
-	
-	/*fs.readFile(path, 'utf8',function(err,data){
-		console.log('data '+data);
-		console.log('error ' +err);
-	});*/
-
-	/* Esto solo para syncs
-	Q(fs.readFileSync(path, 'utf8'))
-	.then(function(data){
-		console.log('datos'+data);
-	}).fail(function(err){
-		console.err("error "+err);
-	});*/
-
-	
 	/* para funciones async
 	var deferred = Q.defer();
 	fs.readFile(path, "utf-8", function (error, text) {
@@ -33,10 +61,18 @@ var readFile = function (path) {
 	return Q.nfcall(fs.readFile, path, "utf-8");
 };
 
-readFile('./app.js')
+readFile(PATH)
 .then(function(text){
-	console.log("leído "+text)
+	console.log("@@@@ promise PATH - leído: "+text)
+	return readFile(PATH2);
+})
+.then(function(text){
+	console.log("@@@@ promise PATH2 - leído: "+text)
+	return readFile(PATH3);
+})
+.then(function(text){
+	console.log("@@@@ promise PATH3 - leído: "+text)
 })
 .fail(function(error){
-	console.error(error);
+	console.error("@@@@ promise - error: " +error);
 });
